f4015f35df854ad91bd3d877f2a72ca9ad57d28a
Teerapan CSIEDU <teerapan.csiedu@gmail.com>
2018-03-30 13:10:43 +0700

    project init

---
+++ src/main/java/com/example/springprototype/SpringPrototypeApplication.java
@@ -0,0 +1,12 @@
+package com.example.springprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPrototypeApplication.class, args);
+	}
+}
